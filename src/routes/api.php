bc92a860c8003187ec0e237bc3f151f5c1520516
Karla Diaz <karladiaze@hotmail.com>
2026-01-14 17:41:45 +0000

    CREATED estructura api

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,26 +3,31 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventoController;
-
-Route::get('/eventos', [EventoController::class, 'index']);
-Route::post('/eventos', [EventoController::class, 'store']);
-Route::get('/eventos/{id}', [EventoController::class, 'show']);
-Route::put('/eventos/{id}', [EventoController::class, 'update']);
-Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
-
-// Rutas para Ponentes
 use App\Http\Controllers\PonentesController;
-Route::get('/ponentes', [PonentesController::class, 'index']);
-Route::post('/ponentes', [PonentesController::class, 'store']);
-Route::get('/ponentes/{id}', [PonentesController::class, 'show']);
-Route::put('/ponentes/{id}', [PonentesController::class, 'update']);
-Route::delete('/ponentes/{id}', [PonentesController::class, 'destroy']);    
-
-// Rutas para Asistentes
 use App\Http\Controllers\AsistentesController;
+
+
+Route::get('/eventos', [EventoController::class, 'index']); // Recuperar todos los eventos
+Route::get('/eventos/{id}', [EventoController::class, 'show']); //Recuperar un evento específico
+Route::get('/ponentes', [PonentesController::class, 'index']);
+Route::get('/ponentes/{id}', [PonentesController::class, 'show']);
 Route::get('/asistentes', [AsistentesController::class, 'index']);
-Route::post('/asistentes', [AsistentesController::class, 'store']);
 Route::get('/asistentes/{id}', [AsistentesController::class, 'show']);
-Route::put('/asistentes/{id}', [AsistentesController::class,    'update']);
-Route::delete('/asistentes/{id}', [AsistentesController::class, 'destroy']);
+
+/**
+* Rutas privadas
+*/
+Route::middleware('auth:api')->group(function () {
+    Route::post('/eventos', [EventoController::class, 'store']);
+    Route::put('/eventos/{id}', [EventoController::class, 'update']);
+    Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
+    Route::post('/ponentes', [PonentesController::class, 'store']);
+    Route::put('/ponentes/{id}', [PonentesController::class, 'update']);
+    Route::delete('/ponentes/{id}', [PonentesController::class, 'destroy']);
+
+    Route::post('/asistentes', [AsistentesController::class, 'store']);
+    Route::put('/asistentes/{id}', [AsistentesController::class, 'update']);
+    Route::delete('/asistentes/{id}', [AsistentesController::class, 'destroy']);
+});
 
